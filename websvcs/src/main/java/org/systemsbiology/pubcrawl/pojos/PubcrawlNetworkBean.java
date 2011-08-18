71fcb2d3cd669393be96ce81aad9d0427b1685c1
Hector Rovira <hrovira.isb@gmail.com>
2011-08-18 13:46:40 -0700

    cleanup web

--- websvcs/src/main/java/org/systemsbiology/pubcrawl/pojos/PubcrawlNetworkBean.java
+++ websvcs/src/main/java/org/systemsbiology/pubcrawl/pojos/PubcrawlNetworkBean.java
@@ -3,64 +3,62 @@ package org.systemsbiology.pubcrawl.pojos;
 import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.logging.Logger;
 
 /**
- * Created by IntelliJ IDEA.
- * User: aeakin
- * Date: Jul 26, 2011
- * Time: 10:24:08 AM
- * To change this template use File | Settings | File Templates.
+ * @author aeakin
  */
 public class PubcrawlNetworkBean {
+    private static final Logger log = Logger.getLogger(PubcrawlNetworkBean.class.getName());
 
     private final String uri;
     private final String node;
     private final Boolean alias;
 
-    public PubcrawlNetworkBean(HttpServletRequest request){
-          this.uri = request.getRequestURI();
-          String nodeUri = StringUtils.substringAfterLast(this.uri,"node/");
-          if(nodeUri != null && !nodeUri.isEmpty()){
-              System.out.println("nodeUri: " + nodeUri);
+    public PubcrawlNetworkBean(HttpServletRequest request) {
+        this.uri = request.getRequestURI();
+        String nodeUri = StringUtils.substringAfterLast(this.uri, "node/");
+        if (nodeUri != null && !nodeUri.isEmpty()) {
+            log.info("nodeUri: " + nodeUri);
             String[] splits = nodeUri.split("/");
-            if(splits.length > 0){
-                this.node = splits[0].replaceAll("%20"," ");
-                this.alias=false;
-            }else{
-                this.node="";
-                this.alias=false;
+            if (splits.length > 0) {
+                this.node = splits[0].replaceAll("%20", " ");
+                this.alias = false;
+            } else {
+                this.node = "";
+                this.alias = false;
             }
 
-          }else{
-              System.out.println("before substring: " + this.uri);
-              nodeUri = StringUtils.substringAfterLast(this.uri,"node_alias/");
-              System.out.println("nodeUri: " +nodeUri);
-              if(nodeUri!= null){
+        } else {
+            log.info("before substring: " + this.uri);
+            nodeUri = StringUtils.substringAfterLast(this.uri, "node_alias/");
+            log.info("nodeUri: " + nodeUri);
+            if (nodeUri != null) {
                 String[] splits = nodeUri.split("/");
-                if(splits.length > 0){
-                    this.node = splits[0].replaceAll("%20"," ");
-                    this.alias=true;
-                }else{
-                    this.node="";
-                    this.alias=false;
+                if (splits.length > 0) {
+                    this.node = splits[0].replaceAll("%20", " ");
+                    this.alias = true;
+                } else {
+                    this.node = "";
+                    this.alias = false;
                 }
-              }else{
-                  this.node="";
-                  this.alias=false;
-              }
-          }
+            } else {
+                this.node = "";
+                this.alias = false;
+            }
+        }
     }
 
-    public String getUri(){
+    public String getUri() {
         return uri;
 
     }
 
-    public String getNode(){
+    public String getNode() {
         return node;
     }
 
-    public Boolean getAlias(){
+    public Boolean getAlias() {
         return alias;
     }
 }
