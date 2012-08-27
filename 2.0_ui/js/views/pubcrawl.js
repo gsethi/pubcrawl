926c884742502b3c90eaa739654c9d2e3d073dc4
aeakin <aeakin@hiking.systemsbiology.net>
2012-08-27 12:03:30 -0700

    fixed table selection, put into new view

--- 2.0_ui/js/views/pubcrawl.js
+++ 2.0_ui/js/views/pubcrawl.js
@@ -110,7 +110,7 @@ PC.PubcrawlView =  Backbone.View.extend({
     showNetworkView: function(selector, view) {
         if (this.networkView)
             this.networkView.close();
-        $(selector).html(view.render($(selector).width(),$(window).height()-80).el);
+        $(selector).html(view.render($(selector).width(),$(window).height()-100).el);
         this.networkView = view;
         return view;
     },
@@ -118,7 +118,7 @@ PC.PubcrawlView =  Backbone.View.extend({
     showNodeFilterListView: function(selector, view){
         if(this.nodeFilterListView)
             this.nodeFilterListView.close();
-        $(selector).html(view.render($(selector).width(),$(window).height() - 100).el);
+        $(selector).html(view.render($(selector).width(),$(window).height() - 150).el);
         this.nodeFilterListView = view;
         this.nodeFilterListView.triggerNetworkFilter();
         return view;
--- 2.0_ui/js/views/queryFilter.js
+++ 2.0_ui/js/views/queryFilter.js
@@ -16,62 +16,17 @@
                 return this;
             }
 
+            this.tableView = new PC.TableView({model: this.model.tableData});
+            this.$el.find("#queryFilterTableView").html(this.tableView.render().el);
 
 
-            //now make data table for modal window
-            var table = '<table class="table table-striped table-bordered" id="queryFilterTable">'+
-                '<thead><tr><th style="width: 5%"><input id="check_all" type="checkbox"/></th><th style="width: 20%">Name</th><th style="width: 40%">Aliases</th>'+
-                '<th style="width: 10%">Term Single Count</th>' +
-                '<th style="width: 10%">Term Combo Count</th><th style="width: 15%">NMD</th></tr></thead><tbody>';
-            $.each(this.model.tableData, function(index,item){
-                table+='<tr><td><input class="tableCheckbox" type="checkbox" /></td><td>'+item.name+'</td><td>' + item.alias +
-                    '</td><td>' + item.termcount + '</td>'+
-                    '<td>'+item.combocount + '</td><td>'+item.ngd+'</td></tr>';
-            });
-            table +='</tbody></table>';
-
-
-            this.$el.find("#queryFilterTableView").html(table);
-            var thisView=this;
-           this.oTable=this.$el.find("#queryFilterTable").dataTable({
-               "sDom": "<'row'<'span3'l><'span4'f>r>t<'row'<'span3'i><'span4'p>>",
-               "sPaginationType": "bootstrap",
-               "oLanguage": {
-			"sLengthMenu": "_MENU_ records per page"
-		},
-               "aoColumns":[
-                   { "sSortDataType": "dom-checkbox"},
-                   null,
-                   null,
-                   null,
-                   null,
-                   null
-               ],
-
-               "fnInitComplete"  : function () {
-                   var that=this;
-                         thisView.$el.find('#check_all').click( function() {
-                            $('input', that.fnGetNodes()).attr('checked',this.checked);
-                             thisView.$el.trigger("tableSelectionChange");
-                    } );
-                   this.$('.tableCheckbox').click(function(){
-                       thisView.$el.trigger("tableSelectionChange");
-                   })
-               }
-           });
-
-            if(this.model.plotData == null || this.model.plotData.length < 1){
-                this.$el.append("<h4>No datapoints found for the search term. Please try a new search.</h4>");
-            }
-            else{
             var histOptions = {startLabel: "Start NMD:", endLabel: "End NMD:", startId: "startNMD", endId: "endNMD",
                 xAxis_Label: "Normalized Medline Distance(NMD)", yAxis_Label: "# of Genes",
                 width: 700, height: 300};
             this.histogramView = new PC.HistogramFilterView({config:histOptions, model: this.model.plotData});
             this.$el.find("#queryFilterHistogramView").html(this.histogramView.render().el);
-            }
 
-          //  $(this.el).modal('show');
+
             return this;
 
         },
@@ -116,13 +71,24 @@
             },
 
         getDataTableTotalSelected: function(){
-             var selected = this.oTable.$('input').filter(
+            var that = this;
+             var selected = {};
+            this.tableView.oTable.$('input').each(
                  function(index){
-                     if(this.checked) return true;
-                     else return false;
+                     if(this.checked){
+                         selected[(that.tableView.oTable.fnGetData(index)[1])]=0;
+                     }
                  });
-            this.selectedNodes = selected;
-            return selected.length;
+
+            var itemsSelected = $.grep(this.model.tableData, function (item,index){
+                if(selected[item.name] != null)
+                    return true;
+                else
+                    return false;
+            });
+
+            this.selectedNodes = itemsSelected;
+            return itemsSelected.length;
         }
 
 
